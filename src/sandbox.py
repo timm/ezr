e3d3bdaf1f477a346b3a36f8c3c104d39733386a
Tim Menzies <timm@ieee.org>
2024-02-14 17:11:07 -0500

    saved}

--- src/sandbox.py
+++ src/sandbox.py
@@ -14,7 +14,7 @@
 #    for three in lst]
 #
 # pc(30)
-import math,sys
+import random,math,sys 
 huge = sys.maxsize
 tiny = 1 / huge
 
@@ -22,25 +22,39 @@ class obj:
   def __init__(i,**d): i.__dict__.update(d)
   def __repr__(i)    : return i.__class__.__name__ +str(i.__dict__)
 
-the = obj(bins=8)
+the = obj(bins=8,seed=31210,data="../data/auto93.csv")
 
-def COL(obj):
+class COL(obj):
   def make(s,n):  return (NUM if s[0].isupper else SYM)(s,n)
 
   def __init__(i,txt=" ",at=0):
-    i.n = 0; i.at = at; i.txt = txt,
-    i.heaven = -1 if s[-1] == "-" else 1
-    i.isGoal = s[-1] in "+-!"
+    i.n = 0; i.at = at; i.txt = txt
+    i.heaven = -1 if txt[-1] == "-" else 1
+    i.isGoal = txt[-1] in "+-!"
 
-class SYM(obj):
-  def __init__(i,**kw): COL.__init__(**kw); i.has = {}
+  def adds(i,lst): [i.add(x) for x in lst];return i
+
+  def bins(i,rowss):
+    return i.bins1(sorted([(row[i.at], klass) 
+                           for klass,rows in rowss.items() 
+                           for row in rows if row[i.at] != "?"]))
+
+class SYM(COL):
+  def __init__(i,**kw): super().__init__(**kw); i.has = {}
   def add(i,x): i.n += 1; i.has[x] = 1 + i.has.get(x,0)
-  def bin(i,x): return x
+  def norm(i,x): return x
   def ent(i)  : return -sum(v/i.n*math.log(v/i.n,2) for _,v in i.has.items() if v>0)
+  def bin1(i,xys,goal):
+    c={}
+    for x,y in xys:   
+      k = y==goal
+      c[k] = c.get(k,{})
+      c[k][x] = c[k].get(x,0) + 1
+    return sorted([(ent(c[k]),k) for k in c])[0] 
 
-def NUM(obj):
+class NUM(COL):
   def __init__(i,**kw):
-    COL.__init__(**kw)
+    super().__init__(**kw)
     i.mu,i.m2,i.sd,i.lo,i.hi = 0,0,0, huge, -huge
 
   def add(i,x):
@@ -55,14 +69,20 @@ def NUM(obj):
 
   def norm(i,x): return x=="?" and x or (x - i.lo) / (i.hi - i.lo + tiny)
 
-  def bin(i,x):
-    n = (i.hi - i.lo)/the.bins
-    return x if x=="?" else min(the.bins-1, int((x - i.lo) / n))
-
-class ROW(obj):
-  def __init__(i,lst, data): i.cells,i.data = lst,data
-
-  def bin(i): i.bins = [col.bin(x) for col,x in zip(i.data.cols,i.cells)]
+  def bins1(i,xys,goal):
+    n1,n2,c1,c2 = 0,len(xys),{},{}
+    for x,y in xys:   
+      k = y==goal
+      c2[k] = c2.get(k,0) + 1
+    small,min = len(xys)**.5, ent(c2) 
+    for n,(x,y) in enumerate(xys):
+      n2    -= 1; n1 += 1
+      c2[y] -= 1; c1[y] = c1.get(y,0) + 1
+      if n1>small and n2>small and x!=xys[n+1][0]:
+        e=(n1*ent(c1) + n2*ent(c2))/(n1+n2)
+        if e < min:
+          min,cut = e,x 
+    return (min,cut)
 
 class DATA(obj):
   def __init__(i,lsts=[],order=False):
@@ -71,12 +91,48 @@ class DATA(obj):
     i.ys   = {col.at:col for col in i.cols if col.isGoal}
     i.rows = []
     [i.add(row) for row in rows]
-    if order: i.ordered()
+    if order: i.rows = sorted(i.rows, key=i.d2h)
 
-  def add(i,row):
-    row = row if isinstance(row,ROW) else ROW(row,i)
+  def add(i,row): 
     i.rows += [row]
     [col.add(x) for x,col in zip(row.cells,i.cols) if x != "?"]
 
   def clone(i, rows=[], order=False):
     return DATA([[col.txt for col in i.cols]] + rows, order=order)
+
+  def d2h(i,row):
+    n,d=0,0
+    for col in i.ys.values():
+      d += abs(col.norm(row[col.at]) - col.heaven)**2
+      n += 1
+    return (d/n)**.5
+
+#-----------------------------------------------------
+def ent(d):
+  n = sum(d.values())
+  return - sum(v/n*math.log(v/n,2) for v in d.values() if v>0)
+
+def coerce(s):
+  try: return ast.literal_eval(s)
+  except Exception: return s
+
+def csv(file=None):
+  with file_or_stdin(file) as src:
+    for line in src:
+      line = re.sub(r'([\n\t\r"\’ ]|#.*)', '', line)
+      if line: yield [coerce(s.strip()) for s in line.split(",")]
+
+#-----------------------------------------------------     make saved
+      
+class Eg: 
+  def all():
+    sys.exit(sum(1 if getattr(Eg,s)()==False else 0 
+                 for s in dir(Eg) if s[0] !="_" and s !="all"))
+    
+  def nums():  
+    assert 6.61  < NUM().adds([x**.5 for x in range(100)]).mu  < 6.62
+    assert 13.31 < NUM().adds([46,	69	,32,	60,	52	,41]).sd < 13.32
+
+if __name__ == "__main__":  
+  random.seed(the.seed)
+  getattr(Eg, next(iter(sys.argv), 1), Eg.all)()
