2299c0a6a1d096ef80d86caa60292c448674a204
Tim Menzies <timm@ieee.org>
2024-03-15 01:25:35 -0400

    saved}

--- src/ediv.py
+++ src/ediv.py
@@ -6,25 +6,23 @@ def ediv(lst, tiny=2):
     lhs,rhs = Counter(), Counter(y for (_,y) in xys)
     e0,n0   = entropy(rhs)
     at,cut  = None, None
-    a       = xys[0][0]
-    z       = xys[-1][0]
     for j,(x,y) in enumerate(xys): 
       if n(lhs) > tiny and n(rhs) > tiny: 
         e1,n1 = entropy(lhs)
         e2,n2 = entropy(rhs)
         maybe = (n1*e1 + n2*e2)/n0
         if maybe < least: 
-          cut,at,least,span = j,x,maybe,(a,x,z)
+          cut,at,least,one,two = j,x,maybe,(xys[0][0],x),(x,xys[-1][0])
       rhs[y] -= 1
       lhs[y] += 1 
-    return cut,at,span
+    return cut,at,one,two
     
   # gain  = e0 - maybe
   #         delta = log2(3**k0-2)-(ke0- ke(rhs)-ke(lhs))
           # if gain >= (log2(n0-1) + delta)/n0: 
   #----------------------------------------------
   def recurse(this, cuts):
-    cut,at = div(this)
+    cut,at,one,two = div(this)
     if cut: 
       recurse(this[:cut], cuts)
       recurse(this[cut:], cuts)
