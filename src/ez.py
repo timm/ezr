c223442982d241fb1f8191c850e35a8c02b701a7
andre-motta <alustos@ncsu.edu>
2024-04-26 18:20:21 -0400

    missing reports

--- src/ez.py
+++ src/ez.py
@@ -18,7 +18,7 @@ OPTIONS:
      -d --discretizationRange    number of bins when discretizing numerical data for SNEAK = 8
      -e --effectSize  non-parametric small delta = 0.2385
      -E --Experiments number of Bootstraps       = 256
-     -f --file        csv data file name         = '../data/xomo_osp2.csv'  
+     -f --file        csv data file name         = '../data/SS-J.csv'  
      -F --Far         far search outlier control = .95 
      -h --help        print help                 = false
      -H --Half        #items for far search      = 256
