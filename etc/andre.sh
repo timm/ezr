3c17d734fdb6b087e21ac4705bec28c0b815fefd
Tim Menzies <timm@users.noreply.github.com>
2024-04-26 22:54:10 -0400

    as

--- etc/andre.sh
+++ etc/andre.sh
@@ -1,13 +1,21 @@
-D="/Users/timm/gits/andre/ez/out/SNEAK"
+D="/Users/timm/gits/andre/ez/out/SNEAKB"
 for f in $D/*.out
 do  
   echo ""
   tac $f | gawk -F, 'NF>7' $f | 
-  sort -t, -n -k 1 -k 2 -k 3   | 
-  grep -v rand | 
-  grep -v 2rrp | 
-  grep -v base | grep -v bonr | grep -v xSNEAK | grep -v rrp|
+  sort -t, -n -k 1 -k 2 -k 3   |#grep -v xrand | grep -v x2rrp | grep -v xbase | grep -v xbonr | grep -v xSNEAK | grep -v xrrp|
   gawk -F, 'BEGIN {_=" , "} {gsub(/[ \t\n]/,"")}  {if (! ($2  in a)) a[$2]= $1 _ $2 _ $3 _ $4} END {for(i in a) {print a[i]}; }' | sort -n 
-done #
-  #gawk -F, '{a[$2][$1]++} END {for(i in a) for (j in a[i]) {print i,j,a[i][j]}}'
+done | 
+  gawk -F, '/^$/{++para} {a[$2][$1]++;  N[$2][$1][para]=$3 } END {
+     for(i in a) 
+        for (j in a[i]) 
+          print i,j,int(100*a[i][j]/31), "$", int(.5+mu(N[i][j])), int(+5+sd(N[i][j])) }
+   function mu(a,   i,n) {
+     for(i in a) n+= a[i]
+     return n/length(a) }
 
+  function sd(a,   i,n,nud) {
+    mid=mu(a)
+    for(i in a) n+=(a[i] - mid)^2
+    return sqrt(n/(length(a) - 1 + 0.0001)) }'
+#
