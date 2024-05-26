2820e850119370d8c5a82c25da887ecc7937cd7d
timm <timm@ieee.org>
2024-05-25 22:36:24 -0400

    as

--- etc/nums.py
+++ etc/nums.py
@@ -1,11 +1,12 @@
 from statistics import NormalDist
+  
+def lin_phi(z): return 1 - 0.5*2.718**(-0.717 * z - 0.416 * z * z)
 
-def lin_phi(x, mu=0, sigma=1):
+def phi(x, mu=0, sigma=1, cdf=lin_phi):
   z = (x - mu) / sigma
-  cdf = lambda z: 1 - 0.5*2.718**(-0.717 * z - 0.416 * z * z)
   return cdf(z) if z>=0 else 1 - cdf(-z)
 
 for x in range(-20,20):
-  got =  lin_phi(x, mu=0, sigma=6)
+  got =  phi(x, mu=0, sigma=6)
   want = NormalDist(mu=0, sigma=6).cdf(x)
   print(x, ', '.join([str(round(z,3)) for z in [got, want, 100*(abs(got-want))/want]]))
