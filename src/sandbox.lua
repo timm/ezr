6e72458e810a9f7a6ba7628e87088a9941ad7b12
timm <timm@ieee.org>
2024-06-25 21:29:58 -0400

    typos

--- src/sandbox.lua
+++ src/sandbox.lua
@@ -1,10 +1,12 @@
 local the={bins=17, cohen=0.35}
 local big=1E30
 
+-- move to "?"sort
+
 function train(it,    data)
   data = {rows={}, cols=nil}
   for row in it do
-    if data.cols then body(data,row) else data.cols=head(row) end end 
+    if data.cols then push(data.rows,row) else data.cols=head(row) end end 
   return data end
 
 function head(row,    cols)
@@ -16,30 +18,25 @@ function head(row,    cols)
                       else cols.x[k] = true end end
   return cols end 
 
-function body(data,row)
-  push(data.rows,row)
-  for k,v in pairs(row) do
-    if x ~= "!" then push(data.cols.has[k], v) end end end
-
 function bin(name,pos) return {name=name,pos=pos,lo=big,hi= -big,n=0} end
 
 function bins(data)
   bins={}
   for k,t in pairs(data.cols.has) do
     table.sort(t)
-    (cols.num[k] and numBins or symBins)(data,k,t,tmp) end end 
+    (cols.num[k] and numBins or symBins)(data.cols.names[k],k,t,tmp) end end 
 
-function numBins(data,k,t,bins,     big,dull,b,out) 
+function numBins(name,k,t,bins,     big,dull,b,out) 
   big  = #t/the.bins
   dull = (t[#t] - t[1])/2.58 * the.cohen
   tmp  = {}
-  b    = push(bins, push(tmp, bin(data.cols.names[k], k)))
+  b    = push(bins, push(tmp, bin(name, k)))
   for k,v in pairs(t) do
     if b.n > big and b.hi - b.lo > dull and #t - k > big then
-      b = push(bins, push(tmp, bin(data.cols.names[k],k))) end
+      b = push(bins, push(tmp, bin(name, k))) end
     b.n  = b.n + 1
-    b.lo = min(b.lo,v)
-    b.hi = max(b.hi,v) end
+    b.lo = math.min(b.lo,v)
+    b.hi = math.max(b.hi,v) end
   tmp[1].lo = - big
   tmp[#tmp].hi = big
   for k = 2,#t do tmp[k].lo = tmp[k-1].hi end
