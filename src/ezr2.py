aed6b916459a7cca0f03c0fbe99ce2591fd04993
timm <timm@ieee.org>
2024-07-28 14:59:53 -0400

    typos

--- src/ezr2.py
+++ src/ezr2.py
@@ -7,7 +7,7 @@ from fileinput import FileInput as file_or_stdin
 from dataclasses import dataclass, field, fields
 from typing import Any, List, Dict, Type
 from math import log,cos,sqrt,pi
-import re,sys,random
+import re,sys,random,inspect
 
 R  = random.random ##
 
@@ -54,63 +54,62 @@ class NUM(COL):
   hi : number = -1E32
   goal : number = 1
 
-  def norm(self, x): return x=="?" and x or (x - self.lo) / (self.hi - self.lo + 1E-32)
-
-
 @dataclass
 class COLS:
-  names: List = LIST()  # column names
+  names: List   # column names
   all  : List = LIST()  # all NUMS and SYMS
   x    : List = LIST()  # independent COLums
   y    : List = LIST()  # depedent COLumns
 
-  def init(self : COLS, row:row) -> COLS:
-    self.names = row
-    for at,txt in enumerate(row): self.init1(at,txt, txt[0],txt[-1])
-    return self
+  def __post_init__(self:COLS) -> None:
+    for at,txt in enumerate(self.names): 
+      a,z = txt[0],txt[-1]
+      col = (NUM if a.isupper() else SYM)(at=at,txt=txt)
+      self.all.append(col)
+      if z != "X":
+        (self.y if z in "+-" else self.x).append(col)
+        if z=="-": col.goal = 0
    
-  def init1(self:COLS, at,txt, a,z):
-    col = (NUM if a.isupper() else SYM)(at=at,txt=txt)
-    self.all += [col]
-    if z != "X":
-      (self.y if z in "+-" else self.x).append(col)
-      if z=="-": col.goal = 0
-
-  def add(self : COLS, row):
-    for cols in [self.x, self.y]:
-      for col in cols: 
-        row[col.at] = col.add(row[col.at])
-    return row
-
 @dataclass
 class DATA:
-  cols   : COLS = None         # summaries of rows
-  rows   : rows = LIST() # rows
+  cols : COLS = None         # summaries of rows
+  rows : rows = LIST() # rows
 
-  def clone(self : DATA, rows:rows==[]) -> DATA:
+  def clone(self:DATA, rows:rows=[]) -> DATA:
     return DATA().add(self.cols.names).adds(rows)
 
-  def sorted(self:DATA):
-    self.rows = sorted(self.rows, key=lambda r: self.d2h(r))
-    return self
-
-  def adds(self:DATA, src): 
-    [self.add(row) for row in src]; return self
-
-  def add(self,row):
-    if    self.cols: self.rows += [self.cols.add(row)]
-    else: self.cols = COLS().init(row)
-    return self
-
 #---------------------------
-import inspect
-def inn(fun): print(1)
 def of(fun):
-    spec = inspect.getfullargspec(fun).annotations
-    cls  = globals()[spec['self']]
-    setattr(cls, fun.__name__, fun)
-    return fun
+  self = inspect.getfullargspec(fun).annotations['self']
+  setattr(globals()[self], fun.__name__, fun)
+  return fun
+
+#----------------------------
+# misc methods
+@of
+def norm(self:NUM, x): 
+  return x=="?" and x or (x - self.lo) / (self.hi - self.lo + 1E-32)
+
 #---------------------------
+@of
+def adds(self:DATA, src): 
+  [self.add(row) for row in src]; return self
+
+@of
+def add(self:DATA,row):
+  if    self.cols: self.rows += [self.cols.add(row)]
+  else: 
+    self.cols = COLS(names=row)
+    print("cols",self.cols.x[1])
+  return self
+
+@of
+def add(self:COLS, row):
+  for cols in [self.x, self.y]:
+    for col in cols: 
+        row[col.at] = col.add(row[col.at])
+  return row
+
 @of
 def add(self:COL, x:any) -> any:
   if x != "?":
@@ -144,14 +143,7 @@ def _coerce(x:str) -> number:
 # ------------------
 # guess
 @of
-def any(self:DATA, fun:Callable=None) -> row:
-  fun = fun or (lambda col: col.any())
-  out = ["?" for _ in self.cols.all]
-  for col in self.cols.x: out[col.at] = fun(col)
-  return out
-
-@of
-def any(self:SYM) -> any: 
+def guess(self:SYM) -> any: 
   r = R()
   for x,n in self.has.items():
     r -= n/self.n
@@ -159,45 +151,55 @@ def any(self:SYM) -> any:
   return self.mode
 
 @of
-def any(self:NUM) -> number:
+def guess(self:NUM) -> number:
   while True:
     x1 = 2.0 * R() - 1
     x2 = 2.0 * R() - 1
     w = x1*x1 + x2*x2
-    if w < 1: break
-  x=x1   
-  w=sqrt((-2*log(w))/w)
-  y1 = x*w
-  return m + y1*s
-
-@of
-def explore(self:DATA, other:DATA):
-  out=self.any()
-  for coli,colj in zip(self.cols.x, other.cols.x): out[coli.at] = coli.explore(colj)
-  return out
-
-@of
-def exploit(self:DATA, other:DATA):
-  out=self.any()
-  for coli,colj in zip(iself.cols.x, other.cols.x): out[coli.at] = coli.exploit(colj)
-  return out
+    if w < 1: 
+      return self.mu + self.sd * x1 * sqrt((-2*log(w))/w)
 
 @of
 def explot(self:COL, other:COL, n=20):
   key     = lambda x: self.like(x,pr1) - other.like(x,pr2)
   nij     = (self.n + other.n + 2*the.k)
   pr1,pr2 = (self.n + the.k) / nij, (other.n + the.k) / nij
-  return max([self.any() for _ in range(n)], key=key)
+  return max([self.guess() for _ in range(n)], key=key)
 
 @of
 def explore(self:COL, other:COL, n=20):
   key     = lambda x: abs(self.like(x,pr1) - other.like(x,pr2))
   nij     = (self.n + other.n + 2*the.k)
   pr1,pr2 = (self.n + the.k) / nij, (other.n + the.k) / nij
-  return min([self.any() for _ in range(n)], key=key)
+  return min([self.guess() for _ in range(n)], key=key)
+
+@of
+def guess(self:DATA, fun:Callable=None) -> row:
+  fun = fun or (lambda col: col.guess())
+  out = ["?" for _ in self.cols.all]
+  for col in self.cols.x: out[col.at] = fun(col)
+  return out
+
+@of
+def explore(self:DATA, other:DATA):
+  out = self.guess()
+  for coli,colj in zip(self.cols.x, other.cols.x): out[coli.at] = coli.explore(colj)
+  return out
+
+@of
+def exploit(self:DATA, other:DATA):
+  out=self.guess()
+  for coli,colj in zip(iself.cols.x, other.cols.x): out[coli.at] = coli.exploit(colj)
+  return out
 
 #--------------
 # Dist
+@of
+def sorted(self:DATA) -> DATA:
+  print(self)
+  self.rows = sorted(self.rows, key=lambda r: self.d2h(r))
+  return self
+
 @of
 def d2h(self:DATA,row:row) -> number:
   d = sum(abs(c.goal - c.norm(row[c.at]))**2 for c in self.cols.y)
@@ -231,7 +233,7 @@ def dist1(self:NUM, x:number, y:number) -> float:
 @of
 def like(self:DATA, r:row, nall:int, nh:int) -> float:
   prior = (len(self.rows) + the.k) / (nall + the.k*nh)
-  likes = [c.like(r[c.at], prior) for c in the.cols.x if r[c.at] != "?"]
+  likes = [c.like(r[c.at], prior) for c in self.cols.x if r[c.at] != "?"]
   return sum(log(x) for x in likes + [prior] if x>0)
 
 @of
