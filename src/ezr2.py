9d3a8d79a44a8a5dfed16f429095d96cd5f4d840
timm <timm@ieee.org>
2024-07-28 09:43:01 -0400

    add remote methods

--- src/ezr2.py
+++ src/ezr2.py
@@ -45,13 +45,6 @@ class SYM(COL):
   mode : atom=None
   most : int=0
 
-  def any(self) : 
-    r = R()
-    for x,n in self.has.items():
-      r -= n/self.n
-      if r <= 0: return x
-    return self.mode
-
 @dataclass     
 class NUM(COL):
   mu : number =  0
@@ -61,8 +54,8 @@ class NUM(COL):
   hi : number = -1E32
   goal : number = 1
 
-  def any(self) : return self.mu + self.sd*sqrt(-2*log(R())) * cos(2*pi*R())
   def norm(self, x): return x=="?" and x or (x - self.lo) / (self.hi - self.lo + 1E-32)
+
 
 @dataclass
 class COLS:
@@ -110,26 +103,28 @@ class DATA:
     return self
 
 #---------------------------
-def of(cls):
-  def assign(fun):
+import inspect
+def inn(fun): print(1)
+def of(fun):
+    spec = inspect.getfullargspec(fun).annotations
+    cls  = globals()[spec['self']]
     setattr(cls, fun.__name__, fun)
     return fun
-  return assign
 #---------------------------
-@of(COL)
+@of
 def add(self:COL, x:any) -> any:
   if x != "?":
      self.n += 1
      x = self.add1(x)
   return x
 
-@of(SYM)
+@of
 def add1(self:SYM, x:any) -> any: 
   self.has[x] = self.has.get(x,0) + 1
   if self.has[x] > self.most: self.mode, self.most = x, self.has[x]
   return x
 
-@of(NUM)
+@of
 def add1(self:NUM, x:any) -> number: 
   if isinstance(x,str): x = _coerce(x)
   # -- lo,hi
@@ -142,71 +137,113 @@ def add1(self:NUM, x:any) -> number:
   self.sd  = 0 if self.n <2 else (self.m2/(self.n-1))**.5
   return x
 
-@of(NUM)
 def _coerce(x:str) -> number:
   try: return float(x)
   except ValueError: return int(x)
 
+# ------------------
+# guess
+@of
+def any(self:DATA, fun:Callable=None) -> row:
+  fun = fun or (lambda col: col.any())
+  out = ["?" for _ in self.cols.all]
+  for col in self.cols.x: out[col.at] = fun(col)
+  return out
+
+@of
+def any(self:SYM) -> any: 
+  r = R()
+  for x,n in self.has.items():
+    r -= n/self.n
+    if r <= 0: return x
+  return self.mode
+
+@of
+def any(self:NUM) -> number:
+  while True:
+    x1 = 2.0 * R() - 1
+    x2 = 2.0 * R() - 1
+    w = x1*x1 + x2*x2
+    if w < 1: break
+  x=x1   
+  w=sqrt((-2*log(w))/w)
+  y1 = x*w
+  return m + y1*s
+
+@of
+def explore(self:DATA, other:DATA):
+  out=self.any()
+  for coli,colj in zip(self.cols.x, other.cols.x): out[coli.at] = coli.explore(colj)
+  return out
+
+@of
+def exploit(self:DATA, other:DATA):
+  out=self.any()
+  for coli,colj in zip(iself.cols.x, other.cols.x): out[coli.at] = coli.exploit(colj)
+  return out
+
+@of
+def explot(self:COL, other:COL, n=20):
+  key     = lambda x: self.like(x,pr1) - other.like(x,pr2)
+  nij     = (self.n + other.n + 2*the.k)
+  pr1,pr2 = (self.n + the.k) / nij, (other.n + the.k) / nij
+  return max([self.any() for _ in range(n)], key=key)
+
+@of
+def explore(self:COL, other:COL, n=20):
+  key     = lambda x: abs(self.like(x,pr1) - other.like(x,pr2))
+  nij     = (self.n + other.n + 2*the.k)
+  pr1,pr2 = (self.n + the.k) / nij, (other.n + the.k) / nij
+  return min([self.any() for _ in range(n)], key=key)
+
 #--------------
 # Dist
-@of(DATA)
+@of
 def d2h(self:DATA,row:row) -> number:
   d = sum(abs(c.goal - c.norm(row[c.at]))**2 for c in self.cols.y)
   return (d/len(self.cols.y)) ** (1/the.p)
 
-@of(DATA)
+@of
 def dist(self:DATA, r1:row, r2:row) -> float:
   n = sum(c.dist(r1[c.at], r2[c.at])**the.p for c in self.cols.x)
   return (n / len(self.cols.x))**(1/the.p)
 
-@of(DATA)
-def neighbors(self:data, r1:row, rows:rows=None) -> rows:
+@of
+def neighbors(self:DATA, r1:row, rows:rows=None) -> rows:
   return sorted(rows or self.rows, key=lambda r2: self.dist(r1,r2))
 
-@of(COL)
+@of
 def dist(self:COL, x:any, y:any) -> float:
   return 1 if x==y=="?" else self.dist1(x,y)
 
-@of(SYM)
+@of
 def dist1(self:SYM, x:number, y:number) -> float: return x != y
 
-@of(NUM)
+@of
 def dist1(self:NUM, x:number, y:number) -> float:
   x, y = self.norm(x), self.norm(y)
   x = x if x !="?" else (1 if y<0.5 else 0)
   y = y if y !="?" else (1 if x<0.5 else 0)
   return abs(x-y)
- #--------------
+
+#--------------
 # Bayes
-@of(DATA)
+@of
 def like(self:DATA, r:row, nall:int, nh:int) -> float:
   prior = (len(self.rows) + the.k) / (nall + the.k*nh)
   likes = [c.like(r[c.at], prior) for c in the.cols.x if r[c.at] != "?"]
   return sum(log(x) for x in likes + [prior] if x>0)
 
-@of(SYM)
+@of
 def like(self:SYM, x:any, prior:float) -> float:
   return (self.has.get(x,0) + the.m*prior) / (self.n + the.m)
 
-@of(NUM)
+@of
 def like(self:NUM, x:number) -> float:
   v     = self.sd**2 + 1E-30
   nom   = math.exp(-1*(x - self.mu)**2/(2*v)) + 1E-30
   denom = (2*math.pi*v) **0.5
   return min(1, nom/(denom + 1E-30))
-
-@of(COL)
-def explot(i,j):
-  nij     = (i.n + j.n + 2*the.k)
-  pr1,pr2 = (i.n + the.k) / nij, (j.n + the.k) / nij
-  return max([i.any() for _ in range(20)], key=lambda x: i.like(x,pr1) - j.like(x,pr2))
-
-# XXXX needs explores, explts
-@of(COL)
-def explore(i,j):
-  nij     = (i.n + j.n + 2*the.k)
-  pr1,pr2 = (i.n + the.k) / nij, (j.n + the.k) / nij
-  return min([i.any() for _ in range(20)], key=lambda x: abs(i.like(x,pr1) - j.like(x,pr2)))
 
 def smo(self:data, score=lambda B,R: B-R, ):
   "Sequential model optimization."
@@ -215,12 +252,11 @@ def smo(self:data, score=lambda B,R: B-R, ):
 
   def _guess(todo:rows, done:rows) -> rows:
     cut  = int(.5 + len(done) ** the.N)
-    best = clone(i, done[:cut])
-    rest = clone(i, done[cut:])
-    want = best.exploit(rest)
+    best = self.clone(done[:cut])
+    rest = self.clone(done[cut:])
+    key  = lambda got : - self.dist(got, best.exploit(rest))
     random.shuffle(todo) 
-    key = lambda got : self.dist(want,got)
-    return  sorted(todo[:the.any], key=key, reverse=True) + todo[the.any:]
+    return sorted(todo[:the.any], key=key) + todo[the.any:]
 
   def _smo1(todo:rows, done:rows) -> rows:
     for k in range(the.Last - the.label):
@@ -231,7 +267,7 @@ def smo(self:data, score=lambda B,R: B-R, ):
     return done
 
   random.shuffle(i.rows) # remove any  bias from older runs
-  return _smo1(i.rows[the.label:], _ranked(data.rows[:the.label])
+  return _smo1(i.rows[the.label:], _ranked(data.rows[:the.label]))
 
 #---------------------------
 def csv(file) -> row:
