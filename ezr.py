3beb1e85b5538f917a0ba3d63461f55975def2a5
timm <timm@ieee.org>
2024-09-11 09:12:37 -0400

    more cluster12

--- ezr.py
+++ ezr.py
@@ -860,9 +860,9 @@ class egs:
 
   def clusters12():
     d = DATA().adds(csv(the.train))
-    for k in [1,2,4]:
+    for k in [1]:
       for stop in [12,24,48]:
-        for some in [64,128,256,512]:
+        for some in [64,128,256,512,10000000000]:
           for train,test in xval(d.rows,m=5,n=5,some=some):
             all = d.clone(train)
             cluster = d.cluster(train,stop=stop)
