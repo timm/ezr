b7e57e8674dc49a383300d5d9893b5ffa36e9936
Tim Menzies <tjmenzie@ncsu.edu>
2025-06-15 11:53:52 -0400

    Update data.py

--- oo/data.py
+++ oo/data.py
@@ -1,15 +1,21 @@
-from lib import obj
+import obj
 from Num import Num
 from Sym import Sym
 
-class Data(obj):
+class Data(obj.ezr):
   "Data stores rows and columns."
-  def __init__(i,inits=[]): 
+  def __init__(i,inits=[]):
     inits = iter(inits)
     i.n     = 0
     i._rows = [],    ## rows
     i.cols  = i._cols(next(inits)) ## summaries of rows
     i.adds(inits)
+
+ def _add(i,row,inc,purge):  
+    "Update the rows acolumns"
+    if inc > 0: i._rows.append(row) 
+    elif purge: i._rows.remove(row) # slow for large lists
+    for col in i.cols.all: col.add(row[col.at], inc)
 
   def _cols(i,names):
     "Factory. List[str] -> Dict[str, List[ Sym | Num ]]"
@@ -26,12 +32,10 @@ class Data(obj):
              x     = x,      ## also, hold independents here
              y     = y)      ## also, hold dependent here
 
-  def _add(i,row,inc,purge):  
-    "Update the rows acolumns"
-    if inc > 0: i._rows.append(row) 
-    elif purge: i._rows.remove(row) # slow for large lists
-    for col in i.cols.all: col.add(row[col.at], inc)
+  def mid(i) : 
+    "Central tendancy."
+    return [mid(c) for c in i.cols.all]
 
-  def mid(i)   : return [mid(c) for c in i.cols.all]
-  def spread(i): return [spread(c) for c in i.cols.all]
-
+  def spread(i): 
+    "Deviation from central tendancy."
+    return [spread(c) for c in i.cols.all]
