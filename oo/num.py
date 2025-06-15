0eb646036e6a972c7b447da51777b627d0628235
Tim Menzies <tjmenzie@ncsu.edu>
2025-06-15 11:51:15 -0400

    Update num.py

--- oo/num.py
+++ oo/num.py
@@ -14,6 +14,17 @@ class Num(obj.ezr):
      i.heaven = 0 if  txt.endswith("-") else 1 ## goal. 0,1=min,max
      i.adds(inits)
 
+  def _add(i,n, inc, _):
+    "Update"
+    i.lo, i.hi = min(n,i.lo), max(n,i.hi)
+    if inc < 0 and i.n < 2: 
+      i.sd = i.m2 = i.mu = i.n = 0
+    else:
+      d       = n - i.mu
+      i.mu += inc * (d / i.n)
+      i.m2 += inc * (d * (n - i.mu))
+      i.sd  = 0 if i.n <= 2 else (max(0,i.m2)/(i.n-1))**.5
+
   def mid(i) : 
     "Cental tendancy."
     return i.mu
@@ -25,14 +36,3 @@ class Num(obj.ezr):
   def norm(i, v): 
     "Normalize 0..1 for min..max."
     return v if v=="?" else (v - i.lo) / (i.hi - i.lo + 1/BIG)
-
-  def _add(i,n, inc, _):
-    "Update"
-    i.lo, i.hi = min(n,i.lo), max(n,i.hi)
-    if inc < 0 and i.n < 2: 
-      i.sd = i.m2 = i.mu = i.n = 0
-    else:
-      d       = n - i.mu
-      i.mu += inc * (d / i.n)
-      i.m2 += inc * (d * (n - i.mu))
-      i.sd  = 0 if i.n <= 2 else (max(0,i.m2)/(i.n-1))**.5
