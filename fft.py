4187ec3c6bd72462e42d24d80ce12c0ba1d9a0fc
timm <timm@ieee.org>
2025-07-22 16:33:57 -0400

    ffts v0.1 working

--- fft.py
+++ fft.py
@@ -22,44 +22,43 @@ the= o(**{k:coerce(v) for k,v in re.findall(r"(\w+)=(\S+)", __doc__)})
 
 #---------------------------------------------------------------------
 BIG = 1e32
-def Num(): return o(lo=BIG, hi= -BIG, mu=0, n=0)
-def Sym(): return {}
-def isSym(col): return type(col) is dict
+def Num(at=0,txt=""): return o(at=at,txt=txt,lo=BIG, hi= -BIG, mu=0, n=0)
+def Sym(at=0,txt=""): return o(at=at,txt=txt,has={})
+def Data()          : return o(rows=[], cols=[])
 
-def add(col, v):
-  if v == "?": return v
-  if isSym(col): col[v] = 1 + col.get(v, 0)
+def isSym(col): return "has" in col.__dict__
+def isData(col): return "rows" in col.__dict__
+
+def add(it, v):
+  if v=="?": return v
+  if isSym(it): it.has[v] = 1 + it.has.get(v, 0)
+  elif isData(it):
+    if it.cols: it.rows.append([add(c,v[c.at]) for c in it.cols.all])
+    else: it.cols = dataHeader(v)
   else:
-    print(col)
-    col.n  += 1
-    delta   = v - col.mu
-    col.mu += delta / col.n
-    col.lo  = min(col.lo, v)
-    col.hi  = max(col.hi, v)
+    it.n  += 1
+    delta   = v - it.mu
+    it.mu += delta / it.n
+    it.lo  = min(it.lo, v)
+    it.hi  = max(it.hi, v)
   return v
 
+def adds(src,it=None):
+  for x in src: it = it or Num(); add(it,x)
+  return it 
+  
 #---------------------------------------------------------------------
-def Data(src=[]):
-  data = o(rows=[], cols=None)
-  for row in src: dataAdd(data, row)
-  return data
-
-def dataAdd(data, row):
-  if data.cols : 
-     data.rows.append([add(col,row[c]) 
-                       for c,col in data.cols.all.items()])
-  else: data.cols = dataHeader(row)
-
 def dataClone(data, rows=[]):
-  return Data([data.cols.names] + rows)
+  return adds([[col.txt for col in data.cols.all]] + rows, Data())
 
 def dataHeader(names):
-  cols = o(names=names, all={}, x={}, y={}, klass=None)
+  cols = o(all=[], x=[], y=[], klass=None)
   for c,s in enumerate(names):
-    col = cols.all[c] = Num() if s[0].isupper() else Sym()
+    col = Num(c, s) if s[0].isupper() else Sym(c, s)
+    cols.all += [col]
     if s[-1] == "X": continue
-    if s[-1] == "!": cols.klass = c
-    (cols.y if s[-1] in "!-+" else cols.x)[c] = col
+    if s[-1] == "!": cols.klass = col
+    (cols.y if s[-1] in "!-+" else cols.x).append(col)
   return cols
 
 def dataRead(file):
@@ -68,33 +67,37 @@ def dataRead(file):
     for line in f:
       line = line.strip()
       if line and not line.startswith("#"):
-        dataAdd(data, [coerce(x) for x in line.split(",")])
+        add(data, [coerce(x) for x in line.split(",")])
   return data
 
 #---------------------------------------------------------------------
 def Tree(data, depth=4):
   def _go(data1, d):  
-    if d > 0 and data1.rows:
-      if cuts := [cut for c, col in data1.cols.x.items()
-                      for cut in treeCuts(data1, col, c, data1.rows)]:
+    sub=False
+    if d > 0 and len(data1.rows)>2:
+      if cuts := [cut for col in data1.cols.x
+                      for cut in treeCuts(data1, col, data1.rows)]:
         best, *_, worst = sorted(cuts)
         for _, c, (xlo, xhi), leaf in [best, worst]:
           yes, no = treeKids(data1.rows, c, xlo, xhi)
           for subtree in _go(dataClone(data1, no), d - 1):
+            sub=True
             yield o(c=c, lo=xlo, hi=xhi, left=leaf, right=subtree)
+    if not sub:
+      yield adds([row[data.cols.klass.at] for row in data1.rows])
   yield from _go(data, depth)
 
-def treeCuts(data, col, c, rows):
+def treeCuts(data, col, rows):
   ys = {}
   for row in rows:
-    x, y = row[c], row[data.cols.klass]
+    x, y = row[col.at], row[data.cols.klass.at]
     if x == "?": continue
     k = x if isSym(col) else x <= col.mu  
     if k not in ys: ys[k] = Num()
     add(ys[k], y)
   return [
    (ys[k].mu, # what to sort on
-    c,        
+    col.at,        
     (k,k) if isSym(col) else ((-BIG,col.mu) if k else (col.mu,BIG)),
     ys[k]) for k in ys]
  
@@ -107,14 +110,14 @@ def treeKids(rows, c, xlo, xhi):
   return yes, no
 
 def treeShow(data, t):
-  if not hasattr(t, "c"): print(f"{t.leaf.mu:.2f}")
+  if not hasattr(t, "c"): print(f"{t.mu:.2f}")
   else:
-    name = data.cols.names[t.c]
-    if   t.lo == t.hi:      txt= f"{pre}if {name} == {int(t.hi)}"
-    elif abs(t.lo) == -BIG: txt= f"{pre}if {name} <= {int(t.hi)}"
-    else:                   txt= f"{pre}else if {name} >= {int(t.lo)}"
-    print(f"if {txt} then {t.leaf.mu} else")
-    treeShow(data,right) 
+    name = data.cols.all[t.c].txt
+    if   t.lo == t.hi:      txt= f"{name} == {t.hi}"
+    elif abs(t.lo) == -BIG: txt= f"{name} <= {t.hi:.3f}"
+    else:                   txt= f"{name} >= {t.lo:.3f}"
+    print(f"if {txt} then {t.left.mu} else")
+    treeShow(data,t.right) 
 
 def treeTune(trees, rows):
   def _predict(t, row):
@@ -122,13 +125,16 @@ def treeTune(trees, rows):
       t = t.left if row[t.c]=="?" or t.lo<=row[t.c]<=t.hi else t.right
     return t.mu
   def _score(t):
-    return sum((r[-1]-_predict(t,row))**2 for row in rows) / len(rows)
+    return sum((row[-1]-_predict(t,row))**2 for row in rows) / len(rows)
   return min(trees, key=_score)
 
 #---------------------------------------------------------------------
 def eg_h(): print(__doc__)
 
-def eg__eg():
+def eg__data():
+  for col in dataRead(the.file).cols.all: print(col)
+
+def eg__tune():
   data = dataRead(the.file)
   trees = list(Tree(data))
   best = treeTune(trees, data.rows)
