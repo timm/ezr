f096604547d45d611fd9bf5a98f5970d70917cbf
timm <timm@ieee.org>
2025-08-05 23:38:54 -0400

    adas

---
+++ danger/cycle.awk
@@ -0,0 +1,8 @@
+BEGIN {
+
+function k(lst,a,x) { lst[a][x] = 0 }
+function Data(a) {
+  n[a]
+  na = mua = m2a = sda = 0
+  nb = mub = m2b = sdb = 0
+  loa, hia 
--- danger/cycle.lua
+++ danger/cycle.lua
@@ -1,5 +1,7 @@
-Num,Sym,Data={},{},{}
-function new(kl,t) kl.__index=kl; return setmetatable(t,kl) end
+Num,Sym,Data,Cols = {},{},{},{}
+
+function new(kl,t) 
+  kl.__index=kl; return setmetatable(t,kl) end
 
 function Data:new(txts) 
   return new(Data,{n=0, rows={}, cols=Cols:new(txts)}) end
@@ -55,13 +57,42 @@ function Num:add(v, inc)
     self.m2 = self.m2 + inc * (d * (v - self.mu))
     self.sd = self.n < 2 and 0 or (math.max(0,self.m2)/(self.n-1))^0.5 end end
 
+function atom(s,     _fn)
+  _fn = function(s) if s=="nil" then return nil  end
+          return s=="true" or s ~="false" and s or false end
+  return math.tointeger(s) or tonumber(s) or _fn(s:match'^%s*(.*%S)') end
 
-done, best, rest = nil,nil,nil
+ function atoms(s,    t)
+  t={}
+  for s1 in s:gsub("%s+", ""):gmatch("([^,]+)") do t[1+#t]=atom(s1) end
+  return t end
 
-for row in csv(the.file) do
-   if not done then
-     done = Data(row), Data(row), Data(row)
-   else
-     if done.n <= 4 then done:add(row) end
-     if done.n == 4 then
-       tabel.sort(done.rows, Y) end
+function csv(src)
+  src = src=="-" and io.stdin or io.input(src)
+  return function(      s)
+    s = io.read()
+    if s then return atoms(s) else io.close(src) end end end
+
+function shuffle(t,      u,j)
+  for i = #t,2,-1 do j=math.random(i); t[i],t[j] = t[j],t[i] end
+  return t end
+
+function Guess:new() 
+  local out = new(Guess,{best=nil, rest=nil, labeled=nil})
+  out.Y = function(row) return disty(
+                    Y=fun) end
+
+function Guess:add(row)
+    if not labeled then labeled = Data:new(row) end
+    if #labeled.n <= 4 then labeled:add(row) end
+    if #labeled.n == 4 then
+      table.sort(labeled.rows, function
+function main(rows)
+  local done, best, rest = nil,nil,nil
+  for row in csv(the.file) do
+    if not done then
+      done = Data(row), Data(row), Data(row)
+    else
+      if done.n <= 4 then done:add(row) end
+      if done.n == 4 then
+          tabel.sort(done.rows, Y) end
