99e0e3459966bacbd7876a4b7c59d7e42eb7e5ee
timm <timm@ieee.org>
2025-08-11 22:41:45 -0400

    asdas

--- ezr/dist.py
+++ ezr/dist.py
@@ -4,7 +4,8 @@ from ezr.data import *
 #--------------------------------------------------------------------
 def dist(src) -> float:
   "general distance function"
-  d,n = 0,0; for v in src: n,d = n+1, d + v**the.p;
+  d,n = 0,0
+  for v in src: n,d = n+1, d + v**the.p;
   return (d/n) ** (1/the.p)
 
 def disty(data:Data, row:Row) -> float:
--- ezr/rq.py
+++ ezr/rq.py
@@ -18,7 +18,31 @@ def eg__treeSelect():
   the.acq="klass"
   rxs=dict(build10=Num(), build20=Num(), build30=Num(), build40=Num(), build80=Num(), build120=Num(),
            check10=Num(), check20=Num(), check30=Num(), check40=Num(), check80=Num(), check120=Num())
+  for budget in [10,20,30,40,80,120]:
+    the.Budget = budget
+    for _ in range(repeats):
+      rows =shuffle(data.rows)
+      m = len(rows)//2
+      train,test = clone(data,rows[:m]), rows[m:]
+      tmp =likely(train)
+      trainTree  = Tree(clone(train,likely(train)))
+      test = sorted(test,key=lambda r: treeLeaf(trainTree,r).ys.mu)[:the.Check]
+      key1 = f"build{budget}"
+      key2 = f"check{budget}"
+      add(rxs[key1], win(tmp[0]))
+      add(rxs[key2], win(distysort(data,test)[0]))
+  print(' '.join(f"{key} {int(log.mu)}" for key,log in rxs.items()))
 
+def eg__overall():
+  "run"
+  repeats = 20
+  data =Data(csv(the.file))
+  b4 = adds(disty(data,r) for r in data.rows)
+  win = lambda x: int(100*(1- (disty(data,x) - b4.lo)/(b4.mu - b4.lo)))
+  trains,tests=Num(),Num()
+  the.acq="klass"
+  rxs=dict(build10=Num(), build20=Num(), build30=Num(), build40=Num(), build80=Num(), build120=Num(),
+           check10=Num(), check20=Num(), check30=Num(), check40=Num(), check80=Num(), check120=Num())
   for budget in [10,20,30,40,80,120]:
     the.Budget = budget
     for _ in range(repeats):
