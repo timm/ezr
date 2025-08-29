1e722e30b52022bb607a3b8bf679c5ddbdd60db3
timm <timm@ieee.org>
2025-08-28 20:17:52 -0400

    share

--- danger/lite.lua
+++ danger/lite.lua
@@ -1,12 +1,13 @@
-local the = {a=4, b=24, c=5, p=2, file="../../moot/optimize/misc/auto93.csv"}
-
+local b4={}; for x,_ in pairs(_ENV) do b4[x]=x end
 local COLS,DATA,NUM,SYM = {},{},{},{}
-
-local adds, cells, new,atom,map,cat,shuffle,csv,push
+local adds,atom,cat,cats,cells,csv,keys,map,new.push,shuffle,sort
+local the = {
+  a=4, b=24, c=5, p=2, 
+  file="../../moot/optimize/misc/auto93.csv"}
 
 ----------------------------------------------------------------------
 function SYM:new(  at, txt) 
-  return l.new(SYM, {n=0, at=at or 0, txt=txt or "", has={}}) end
+  return new(SYM, {n=0, at=at or 0, txt=txt or "", has={}}) end
 
 function SYM:add(x, inc) 
   if x ~= "?" then 
@@ -20,14 +21,14 @@ function SYM:mid()
     if n > most then mode,most = x,n end end
   return mode end
 
-function Data:mid() return self.mu end 
-function Num:mid() return self.mu end 
+function DATA:mid() return self.mu end 
+function NUM:mid()  return self.mu end 
 
 function NUM:add(x, inc) 
   if x ~= "?" then
     local d = x - self.mu
     self.n  = self.n  + inc
-    self.mu = self.mu + inc * (d / self.mu) 
+    self.mu = self.mu + inc * (d / self.mu) end
   return x end
 
 function NUM:new(  at, txt) 
@@ -43,44 +44,68 @@ function COLS:new(names,     all,x,y,col)
   return new(COLS, {names=names, all=all, x=x, y=y}) end
 
 function DATA:new(t) 
-  return l.new(DATA, {rows={}, mid=nil, cols=COLS:new(t)}) end
+  return new(DATA, {rows={}, mid=nil, cols=COLS:new(t)}) end
 
 function DATA:clone(  rows) 
   return DATA:new():from({self.cols.names}):from(rows) end
 
+-------------------------------------------------------------------------------
 function adds(t, it)
   it = it or NUM()
-  for _,x in pairs(t or {}) do self:add(row) end
+  for _,x in pairs(t or {}) do it:add(row) end
   return it end
 
+function atom(s,      f)    
+  f = function(s) return s=="true" or s~="false" end
+  return math.tointeger(s) or tonumber(s) or f(s:match'^%s*(.*%S)') end
+
+function cat(x)
+  f, is = string.format, type(x)
+  return is=="table" and "{" ..table.concat(cats(t,tostring),", ").. "}" or
+         is=="number" and f(x==math.floor(x) and "%.0f" or "%.3f", x) or 
+         f("%s",x) end
+
+function cats(t,f)
+  if #t > 0 then return map(t,cat) end
+  u={}; for _,k in pairs(keys(t)) do u[k]=f(":%s %s",k,cat(t[k])) end
+  return sort(u) end
+
 function cells(s,    t)
-  t={}; for s1 in s:gsub("%s+", ""):gmatch("([^,]+)") do t[1+#t]=atom(s1) end
+  t = {}; for s1 in s:gsub("%s+", ""):gmatch("([^,]+)") do t[1+#t]=atom(s1) end
   return t end
 
-function new(kl,t)  kl.__index = kl; return setmetatable(t,kl) end
-function atom(s)    return math.tointeger(s) or tonumber(s) or s:match'^%s*(.*%S)' end
-function map(t,fn)  local u={}; for _,x in ipairs(t) do u[1+#u]=fn(x) end; return u end
-function cat(t)     return "{" .. table.concat(map(t,tostring),", ") .. "}" end
+function csv(src,      s)
+  src = io.input(src)
+  return function()
+    s = io.read(); if s then return cells(s) else io.close(src) end end end
+
+function keys(t,  u) 
+  u = {}
+  for k,_ in pairs(t) do if tostring(k):sub(1,1) ~= "_" then u[1+#u]=k end end
+  return sort(u) end
+
+function map(t,f,   u) 
+  u={}; for _,x in pairs(t) do u[1+#u]=f(x) end; return u end end
+
+function new(kl,t) kl.__index = kl; return setmetatable(t,kl) end
+
+function push(t,x) t[1+#t]=x; return x end
 
 function shuffle(t,    j) 
   for i = #t,2,-1 do j=math.random(i); t[i],t[j] = t[j],t[i] end; return t end
 
-function csv(src,      s,i)
-  i, src = -1, io.input(src)
-  return function()
-    s = io.read(); if s then i=i+1; return i,cells(s) else io.close(src) end end end
+function sort(t,fn) table.sort(t,fn); return t end
 
-function push(t,x) t[1+#t]=x; return x end
-
-
-for i,row in csv(the.file) do 
-  if i==0 then
-    for i,s in pairs(row) do 
-      if s:find:"^[A-Z]" then 
-        nums[i] = {big, -big}
-        if s:find"-$" then y[i]=0 end
-        if s:find"+$" then y[i]=1 end 
-  else rows[1+#rows]=row end
-
-B, R, best, rest = 0, 0, {}, {}
-for i,row in pairs(shuffle(rows)) do
+-- for i,row in csv(the.file) do 
+--   if i==0 then
+--     for i,s in pairs(row) do 
+--       if s:find:"^[A-Z]" then 
+--         nums[i] = {big, -big}
+--         if s:find"-$" then y[i]=0 end
+--         if s:find"+$" then y[i]=1 end 
+--   else rows[1+#rows]=row end
+--
+-- for x in _ENV do print(x) end
+--
+-- B, R, best, rest = 0, 0, {}, {}
+--for i,row in pairs(shuffle(rows)) do
