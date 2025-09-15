bb3a728b5e7ee13ea01255c73680490e3eef4a29
timm <timm@ieee.org>
2025-09-15 11:25:16 -0400

    optimizing stats

--- ezr/stats.py
+++ ezr/stats.py
@@ -1,9 +1,9 @@
 from types import SimpleNamespace as o
-from typing import Any,List,Iterator
+import bisect
 
-Number = int|float
-Atom   = Number|str|bool
-Row    = List[Atom]
+Qty = int|float
+Atom   = Qty|str|bool
+Row    = list[Atom]
 
 def Confuse(): 
   "Create a confusion stats for classification matrix."
@@ -20,7 +20,7 @@ def confuse(cf:Confuse, want:str, got:str) -> str:
   cf.total += 1
   return got
 
-def confused(cf:Confuse, summary=False) -> List[Confuse]:
+def confused(cf:Confuse, summary=False) -> list[Confuse]:
   "Report confusion metric statistics."
   p = lambda y, z: round(100 * y / (z or 1e-32), 0)  # one decimal
   def finalize(c):
@@ -43,7 +43,7 @@ def confused(cf:Confuse, summary=False) -> List[Confuse]:
                 key=lambda cf: cf.fn + cf.tp)
 
 #------------------------------------------------------------------------------
-def same(x:list[Number], y:list[Number],Ks=0.95,Delta="smed") -> bool: 
+def same(x:list[Qty], y:list[Qty],Ks=0.95,Delta="smed") -> bool: 
   "True if x,y indistinguishable and differ by just a small effect."
   x, y = sorted(x), sorted(y)
   n, m = len(x), len(y)
@@ -53,7 +53,7 @@ def same(x:list[Number], y:list[Number],Ks=0.95,Delta="smed") -> bool:
     gt = sum(a > b for a in x for b in y)
     lt = sum(a < b for a in x for b in y)
     return abs(gt - lt) / (n * m)
-
+  
   def _ks():
     "Return max distance between cdf."
     xs = sorted(x + y)
@@ -66,7 +66,7 @@ def same(x:list[Number], y:list[Number],Ks=0.95,Delta="smed") -> bool:
   return _cliffs() <= cliffs and _ks() <= ks * ((n + m)/(n * m))**0.5
 
 #------------------------------------------------------------------------------
-def top(rxs:dict[str,list[Number]], 
+def top(rxs:dict[str,list[Qty]], 
         reverse=False, same=same, 
         eps=0.01, Ks=.95, Delta="smed") -> set:
   "Return the subset of rxs's keys associated with best scores."
@@ -86,4 +86,25 @@ def top(rxs:dict[str,list[Number]],
     its = its[:cut]
   return {k for _, _, k, _ in its}
 
+#------------------------------------------------------------------------------
+def weibulls(m=20,n=20):
+  "FYI  most of the time is in _cliffs"
+  import random, math
+  def weibull(n=100):
+    shape, scale = random.uniform(0.5, 3), random.uniform(1, 4)
+    return [min(10, scale * (-math.log(random.random())) ** (1/shape) * 2.5) for _ in range(n)]
+  return top({x:weibull(n) for x in range(m)})
 
+def profile():
+  import cProfile
+  import pstats
+  pr = cProfile.Profile()
+  pr.enable()
+  weibulls(100,100) # <== 100 treatmetns, each with 100 values. insanely big
+  pr.disable()
+  stats = pstats.Stats(pr)
+  stats.sort_stats('cumulative')
+  stats.print_stats(20)
+
+
+if __name__== "__main__": profile()
