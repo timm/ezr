9a2425e1ea4280e121c33cb1b6d2b1b7a12f1a34
timm <timm@ieee.org>
2026-02-12 17:15:44 -0500

    rules dijusctions

--- ezr/ez.py
+++ ezr/ez.py
@@ -107,8 +107,45 @@ def around(data,row,rows):
 
 #--------------------------------------------------------------------
 # Simple rule generation
-def ruleScore(b, r): return b**2 / (b + r + 1e-30)
+# --- build 1 rile
+def ruleAdd(rule, at, b):
+  spans = rule.setdefault(at, [])
+  for i, (a, z) in enumerate(spans):
+    if a-1 <= b <= z+1:
+      spans[i] = (min(a,b), max(z,b))
+      return
+  spans.append((b, b))
 
+def ruleSeen(rule, at, b):
+  return at in rule and any(a <= b <= z for a, z in rule[at])
+
+# --- query rows with a rule 
+def ruleSelect(data, rule, row):
+  fn = lambda at, a, z: a <= bucket(data.cols.all[at], row[at]) <= z
+  return all(any(fn(at, a, z) for a, z in spans)
+             for at, spans in rule.items())
+
+def ruleSelects(data, rule, rows):
+  return [r for r in rows if ruleSelect(data, rule, r)]
+
+# --- show a rule
+def ruleShowClause(txt, lo, hi):
+  return f"{txt} == {o(lo)}" if lo == hi else f"{o(lo)} <= {txt} <= {o(hi)}"
+
+def ruleShow(rule, data, lo, hi, rows=None):
+  pre = "IF  "
+  for at, spans in rule.items():
+    txt = data.cols.all[at].txt
+    s = " OR ".join(ruleShowClause(txt, lo[at,a], hi[at,b]) for a,b in spans)
+    if rows:
+      rows = ruleSelects(data, {at: spans}, rows)
+      s2 = adds(disty(data, r) for r in rows)
+      print(f"{pre}{s:<35} THEN {o(s2.mu)} ({s2.n})")
+    else:
+      print(f"{pre}{s}")
+    pre = "AND "
+   
+# --- grow rules
 def ruleGrow(data):
   lo, hi = {}, {}
   def stats(rows):
@@ -122,32 +159,27 @@ def ruleGrow(data):
           hi[k] = max(hi.get(k, v), v)
     return d
 
-  def grow(b, r, used):
+  def score(b, r): return b**2 / (b + r + 1e-30)
+
+  def grow(b, r, rule):
     if len(b) > the.leaf and r:
       bd, rd = stats(b), stats(r)
-      if best_k := max((k for k in bd if k[0] not in used), default=None,
-                       key=lambda k: ruleScore(bd[k], rd.get(k, 0))):
-        col = data.cols.all[best_k[0]]
-        match = lambda row: bucket(col, row[col.at]) == best_k[1]
-        b1    = [row for row in b if match(row)]
-        r1    = [row for row in r if match(row)]
-        s     = adds(disty(data, row) for row in b1)
+      if most := max((k for k in bd if not ruleSeen(rule, *k)), default=None,
+                      key=lambda k: score(bd[k], rd.get(k, 0))):
+        at, want = most
+        b1 = ruleSelects(data, {at:[(want,want)]}, b)
+        r1 = ruleSelects(data, {at:[(want,want)]}, r)
+        s  = adds(disty(data, row) for row in b1)
         if s.n >= the.leaf and (len(b1) < len(b) or len(r1) < len(r)):
-          used.add(best_k[0])
-          yield (col.txt, lo[best_k], hi[best_k], s.n, s.mu)
-          yield from grow(b1, r1, used)
-    
+          ruleAdd(rule, at, want)
+          grow(b1, r1, rule)
+
   data.rows.sort(key=lambda r: disty(data, r))
   n = max(int(len(data.rows)**0.5), the.leaf + 1)
-  return list(grow(data.rows[:n], data.rows[n:], set()))
+  rule = {}
+  grow(data.rows[:n], data.rows[n:], rule)
+  return rule, lo, hi
 
-def ruleShow(rules):
-  pre = "IF  "
-  for txt, lo, hi, n, mu in rules:
-    bnd = f"{txt} == {o(lo)}" if lo==hi else f"{o(lo)} <= {txt} <= {o(hi)}"
-    print(f"{pre}{bnd:<35} THEN {o(mu)} ({n})")
-    pre = "AND "
-     
 #--------------------------------------------------------------------
 # lib
 
@@ -241,7 +273,12 @@ def eg__data(f:filename):
 
 def eg__bore(f:filename):
   "best or rest rule generation"
-  ruleShow(ruleGrow(DATA(csv(f))))
+  data = DATA(csv(f))
+  some = clone(data, shuffle(data.rows)[:50])
+  [print(row) for row in some.rows]
+  rule, lo, hi = ruleGrow(some)
+  n = max(int(len(some.rows)**0.5), the.leaf + 1)
+  ruleShow(rule, some, lo, hi, some.rows[:n])
 
 #--------------------------------------------------------------------
 the= OBJ(**{k: cast(v) for k, v in re.findall(r"(\S+)=(\S+)", __doc__)})
